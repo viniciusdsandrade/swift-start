2d49835ab1fee9ee71c40920dc3f53c248bd2cc9
Vinicius <vinicius_andrade2010@hotmail.com>
2024-09-18 17:00:29 -0300

    setup: cardApp

---
+++ CardApp/CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by VINICIUS DOS SANTOS ANDRADE on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
